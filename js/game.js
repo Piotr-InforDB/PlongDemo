ef973502e55ad40d47923b0387baec9e5569f880
piotr <piotr@infordb.com>
2024-03-23 15:43:28 +0100

    Start game on Space Calcualte bounce direction Visualize end of game

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-$(document).on('keydown', movePaddle)
+$(document).on('keydown', interact)
 
 const dom = {
   body: $('body'),
@@ -7,12 +7,13 @@ const icons = {
   plant: `<i class="fa-solid fa-seedling"></i>`,
 }
 const display = {
-  x: 51,
-  y: 21,
+  x: 25 ,
+  y: 15,
   middle_x: null,
   middle_y: null,
 }
 const paddles = {
+  height: null,
   offset: null,
   left: null,
   right: null,
@@ -25,7 +26,7 @@ const ball = {
 const game = {
   on: false,
   interval: null,
-  speed: 15,
+  speed: 12.5,
 }
 
 function init(){
@@ -39,19 +40,21 @@ function init(){
   }
 }
 function resetGame(){
+  displayMessage('Press SPACE to start the game!');
   game.on = false;
-  
+
   display.middle_y = Math.ceil(display.y / 2);
   display.middle_x = Math.ceil(display.x / 2);
   
-  paddles.offset = Math.ceil(display.y / 25);
+  paddles.offset = Math.ceil(display.y / 10);
+  paddles.height = paddles.offset * 2 + 1;
   paddles.left = display.middle_y;
   paddles.right = display.middle_y;
   
   ball.x = display.middle_x;
   ball.y = display.middle_y;
- 
-  if(!game.interval){ game.interval = setInterval(interval, 1000 / game.speed); }
+
+  if(!game.interval){ game.interval = setInterval(interval, 1000 / game.speed) };
 }
 function visualize(){
   const {left, right, offset} = paddles;
@@ -74,10 +77,23 @@ function interval(){
   
   moveBall();
 }
+function displayMessage(message){
+  dom.body.append(`<div class="game-message" >${message}</div>`)
+}
+function clearMessage(){
+  $('.game-message').remove();
+}
 
+function interact(e){
+  if(e.keyCode === 32){
+    clearMessage();
+    game.on = true;
+  }
+
+  if(!game.on){ return; }
+  movePaddle(e);
+}
 function movePaddle(e){
-  game.on = true;
-  
   const { offset } = paddles;
   const target = {
     paddle: null,
@@ -110,27 +126,55 @@ function movePaddle(e){
   visualize();
 }
 function moveBall(){
-  if(!ball.direction){ ball.direction = ([0, 45, 135, 180, 225, 315])[Math.round(Math.random() * 6) - 1]; }
-  
+  if(ball.x === 1 || ball.x === display.x){
+    markPlant(ball.x, ball.y, 'danger');
+    resetGame();
+    return;
+  }
+  if(ball.direction === null){
+    ball.direction = ([0, 45, 135, 180, 225, 315])[Math.round(Math.random() * 6) - 1];
+  }
+
   //Bounce top && Bottom
   if (ball.y === 1 || ball.y === display.y) {
     ball.direction = 360 - ball.direction;
   }
   
   //Bounce sides
-  let bounce;
+  let bounce_y;
   if(ball.x === 2){
-    bounce = ball.y >= (paddles.left - paddles.offset) && ball.y <= (paddles.left + paddles.offset);
-    if(!bounce){ resetGame(); }
+    bounce_y = (ball.y >= (paddles.left - paddles.offset) && ball.y <= (paddles.left + paddles.offset)) ? paddles.left : false;
   }
   else if(ball.x === display.x - 1){
-    bounce = ball.y >= (paddles.right - paddles.offset) && ball.y <= (paddles.right + paddles.offset);
-    if(!bounce){ resetGame(); }
+    bounce_y = (ball.y >= (paddles.right - paddles.offset) && ball.y <= (paddles.right + paddles.offset)) ? paddles.right : false;
   }
   
   //Bounce
-  if(bounce){ ball.direction += 180; }
-  
+  if(bounce_y){
+    const left_side = ball.x === 2;
+
+    let bottom = bounce_y - paddles.offset;
+    let top = bounce_y + paddles.offset;
+    let ball_y = ball.y;
+
+    //Normalize the range;
+    ball_y += -bottom;
+    top += -bottom;
+
+    const bounce_spot_ratio = ball_y / top;
+
+    if(bounce_spot_ratio < .33){
+      ball.direction = left_side ? 315 : 225;
+    }
+    else if(bounce_spot_ratio > .66){
+      ball.direction = left_side ? 45 : 135;
+    }
+    else{
+      ball.direction = left_side ? 0 : 180;
+    }
+
+  }
+
   const angle = ball.direction * (Math.PI / 180);
   ball.x = Math.round(ball.x + Math.cos(angle));
   ball.y = Math.round(ball.y + Math.sin(angle));
@@ -140,8 +184,12 @@ function moveBall(){
 function setPlant(x, y, state){
   $(`.plant[data-x=${x}][data-y=${y}]`).addClass('active');
 }
+function markPlant(x, y, color){
+  $(`.plant[data-x=${x}][data-y=${y}]`).addClass(`text-${color}`);
+}
 function resetPlants(){
-  $(`.plant`).removeClass('active');
+  $(`.plant.text-danger`).removeClass('text-danger');
+  $(`.plant.active`).removeClass('active');
 }
 
 init()
